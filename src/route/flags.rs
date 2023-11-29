cf8af78a691070d3259bbecd8a33a99cccc268fb
Gris Ge <fge@redhat.com>
2023-11-29 15:27:17 +0800

    route: Add support of ONLINK route flags
    The first u8 rtm_flags is sharing with RouteNextHopFlag.
    
    Unit test case included.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- src/route/flags.rs
+++ src/route/flags.rs
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: MIT
 
+use super::next_hops::{
+    RTNH_F_DEAD, RTNH_F_LINKDOWN, RTNH_F_OFFLOAD, RTNH_F_ONLINK,
+    RTNH_F_PERVASIVE, RTNH_F_TRAP, RTNH_F_UNRESOLVED,
+};
+
 const RTM_F_NOTIFY: u32 = 0x100;
 const RTM_F_CLONED: u32 = 0x200;
 const RTM_F_EQUALIZE: u32 = 0x400;
@@ -15,41 +20,65 @@ const RTM_F_OFFLOAD_FAILED: u32 = 0x20000000;
 #[derive(Clone, Eq, PartialEq, Debug, Copy)]
 #[non_exhaustive]
 pub enum RouteFlag {
+    // Kernel also store next hope flags here
+    Dead,
+    Pervasive,
+    Onlink,
+    Offload,
+    Linkdown,
+    Unresolved,
+    Trap,
+    // Next hope flags ends
     Notify,
     Cloned,
     Equalize,
     Prefix,
     LookupTable,
     FibMatch,
-    Offload,
-    Trap,
+    RtOffload,
+    RtTrap,
     OffloadFailed,
     Other(u32),
 }
 
-const ALL_ROUTE_FLAGS: [RouteFlag; 9] = [
+const ALL_ROUTE_FLAGS: [RouteFlag; 16] = [
+    RouteFlag::Dead,
+    RouteFlag::Pervasive,
+    RouteFlag::Onlink,
+    RouteFlag::Offload,
+    RouteFlag::Linkdown,
+    RouteFlag::Unresolved,
+    RouteFlag::Trap,
     RouteFlag::Notify,
     RouteFlag::Cloned,
     RouteFlag::Equalize,
     RouteFlag::Prefix,
     RouteFlag::LookupTable,
     RouteFlag::FibMatch,
-    RouteFlag::Offload,
-    RouteFlag::Trap,
+    RouteFlag::RtOffload,
+    RouteFlag::RtTrap,
     RouteFlag::OffloadFailed,
 ];
 
 impl From<RouteFlag> for u32 {
     fn from(v: RouteFlag) -> u32 {
         match v {
+            RouteFlag::Dead => RTNH_F_DEAD.into(),
+            RouteFlag::Pervasive => RTNH_F_PERVASIVE.into(),
+            RouteFlag::Onlink => RTNH_F_ONLINK.into(),
+            RouteFlag::Offload => RTNH_F_OFFLOAD.into(),
+            RouteFlag::Linkdown => RTNH_F_LINKDOWN.into(),
+            RouteFlag::Unresolved => RTNH_F_UNRESOLVED.into(),
+            RouteFlag::Trap => RTNH_F_TRAP.into(),
+
             RouteFlag::Notify => RTM_F_NOTIFY,
             RouteFlag::Cloned => RTM_F_CLONED,
             RouteFlag::Equalize => RTM_F_EQUALIZE,
             RouteFlag::Prefix => RTM_F_PREFIX,
             RouteFlag::LookupTable => RTM_F_LOOKUP_TABLE,
             RouteFlag::FibMatch => RTM_F_FIB_MATCH,
-            RouteFlag::Offload => RTM_F_OFFLOAD,
-            RouteFlag::Trap => RTM_F_TRAP,
+            RouteFlag::RtOffload => RTM_F_OFFLOAD,
+            RouteFlag::RtTrap => RTM_F_TRAP,
             RouteFlag::OffloadFailed => RTM_F_OFFLOAD_FAILED,
             RouteFlag::Other(i) => i,
         }
