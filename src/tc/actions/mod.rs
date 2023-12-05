a7197f030f68cf081079a8fdabf7956a1fe31cb1
Gris Ge <fge@redhat.com>
2023-12-05 10:12:45 +0800

    tc: Parse `TcMirror.eaction` to `TcMirrorActionType`
    Signed-off-by: Gris Ge <fge@redhat.com>

--- src/tc/actions/mod.rs
+++ src/tc/actions/mod.rs
@@ -7,11 +7,11 @@ pub(crate) mod nat_flag;
 
 pub use self::action::{
     TcAction, TcActionAttribute, TcActionGeneric, TcActionGenericBuffer,
-    TcActionType,
-    TcActionOption,
+    TcActionOption, TcActionType,
 };
 pub use self::mirror::{
-    TcActionMirror, TcActionMirrorOption, TcMirror, TcMirrorBuffer,
+    TcActionMirror, TcActionMirrorOption, TcMirror, TcMirrorActionType,
+    TcMirrorBuffer,
 };
 pub use self::nat::{TcActionNat, TcActionNatOption, TcNat, TcNatBuffer};
 pub use self::nat_flag::TcNatFlag;
