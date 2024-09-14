f5d6cea326616817168639f5bd04869c347e467e
Alexander Chernikov <alexander.chernikov@omniva.com>
2024-09-14 15:22:26 +0000

    Add tunnel_key action.

--- src/tc/actions/mod.rs
+++ src/tc/actions/mod.rs
@@ -16,6 +16,9 @@ pub use self::mirror::{
     TcMirrorBuffer,
 };
 pub use self::nat::{TcActionNat, TcActionNatOption, TcNat, TcNatBuffer};
+pub use self::tunnel_key::{
+    TcActionTunnelKey, TcActionTunnelKeyOption, TcTunnelKey,
+};
 
 mod action;
 mod header;
@@ -23,6 +26,7 @@ mod message;
 mod mirror;
 mod nat;
 mod nat_flag;
+mod tunnel_key;
 
 #[cfg(test)]
 pub mod tests;
