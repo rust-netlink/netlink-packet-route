fa211295b94d51f6eb1a7d0fa4004a2fa34dfd6c
Kunihiro Ishiguro <kunihiro@zebra.dev>
2024-11-08 09:47:27 -0800

    Add weight high order byte.

--- src/nexthop/group.rs
+++ src/nexthop/group.rs
@@ -4,15 +4,15 @@ use netlink_packet_utils::{
     DecodeError, Emitable,
 };
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Default)]
 /// Nexthop Group
 pub struct NexthopGroup {
     /// Nexthop id
     pub id: u32,
     /// Weight of this nexthop
     pub weight: u8,
-    /// Reserved
-    pub resvd1: u8,
+    /// High order bits of weight
+    pub weight_high: u8,
     /// Reserved
     pub resvd2: u16,
 }
@@ -25,7 +25,7 @@ impl Emitable for NexthopGroup {
     fn emit(&self, buffer: &mut [u8]) {
         NativeEndian::write_u32(buffer, self.id);
         buffer[4] = self.weight;
-        buffer[5] = self.resvd1;
+        buffer[5] = self.weight_high;
         NativeEndian::write_u16(&mut buffer[6..8], self.resvd2);
     }
 }
@@ -35,7 +35,7 @@ impl NexthopGroup {
         let grp = Self {
             id: parse_u32(payload)?,
             weight: payload[4],
-            resvd1: payload[5],
+            weight_high: payload[5],
             resvd2: parse_u16(&payload[6..8])?,
         };
         Ok(grp)
