30a511fe12dbd93110746841cd88f1f02b9f8dbd
Till Maas <opensource@till.name>
2025-04-15 13:01:07 +0200

    DO NOT MERGE: Demonstrate lacking test coverage
    To demonstrate that the current test strategy is lacking test coverage,
    break the MinMtu data type without breaking the existing test cases.

--- src/link/attribute.rs
+++ src/link/attribute.rs
@@ -158,7 +158,7 @@ pub enum LinkAttribute {
     GsoMaxSegs(u32),
     GsoMaxSize(u32),
     /// The minimum MTU for the device.
-    MinMtu(u32),
+    MinMtu(i32),
     /// The maximum MTU for the device.
     MaxMtu(u32),
     LinkNetNsId(i32),
@@ -284,8 +284,11 @@ impl Nla for LinkAttribute {
             | Self::CarrierDownCount(value)
             | Self::GsoMaxSegs(value)
             | Self::GsoMaxSize(value)
-            | Self::MinMtu(value)
-            | Self::MaxMtu(value) => NativeEndian::write_u32(buffer, *value),
+            => NativeEndian::write_u32(buffer, *value),
+
+            Self::MinMtu(value) => NativeEndian::write_i32(buffer, *value),
+
+            Self::MaxMtu(value) => NativeEndian::write_u32(buffer, *value),
 
             Self::ExtMask(value) => NativeEndian::write_u32(
                 buffer,
@@ -598,7 +601,7 @@ impl<'a, T: AsRef<[u8]> + ?Sized>
                     .context("invalid IFLA_GSO_MAX_SIZE value")?,
             ),
             IFLA_MIN_MTU => Self::MinMtu(
-                parse_u32(payload).context("invalid IFLA_MIN_MTU value")?,
+                parse_i32(payload).context("invalid IFLA_MIN_MTU value")?,
             ),
             IFLA_MAX_MTU => Self::MaxMtu(
                 parse_u32(payload).context("invalid IFLA_MAX_MTU value")?,
