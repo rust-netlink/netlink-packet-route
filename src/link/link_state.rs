d9426a049aaf765deb7612833b0b563b49f9dbec
Gris Ge <cnfourt@gmail.com>
2026-07-12 15:49:30 +0800

    link: Add FromStr and Display for State
    Signed-off-by: Gris Ge <cnfourt@gmail.com>

--- src/link/link_state.rs
+++ src/link/link_state.rs
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: MIT
 
+use std::fmt;
+
+use netlink_packet_core::DecodeError;
+
 const IF_OPER_UNKNOWN: u8 = 0;
 const IF_OPER_NOTPRESENT: u8 = 1;
 const IF_OPER_DOWN: u8 = 2;
@@ -32,32 +36,64 @@ pub enum State {
 
 impl From<u8> for State {
     fn from(value: u8) -> Self {
-        use self::State::*;
         match value {
-            IF_OPER_UNKNOWN => Unknown,
-            IF_OPER_NOTPRESENT => NotPresent,
-            IF_OPER_DOWN => Down,
-            IF_OPER_LOWERLAYERDOWN => LowerLayerDown,
-            IF_OPER_TESTING => Testing,
-            IF_OPER_DORMANT => Dormant,
-            IF_OPER_UP => Up,
-            _ => Other(value),
+            IF_OPER_UNKNOWN => Self::Unknown,
+            IF_OPER_NOTPRESENT => Self::NotPresent,
+            IF_OPER_DOWN => Self::Down,
+            IF_OPER_LOWERLAYERDOWN => Self::LowerLayerDown,
+            IF_OPER_TESTING => Self::Testing,
+            IF_OPER_DORMANT => Self::Dormant,
+            IF_OPER_UP => Self::Up,
+            _ => Self::Other(value),
+        }
+    }
+}
+
+impl fmt::Display for State {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Unknown => write!(f, "UNKNOWN"),
+            Self::NotPresent => write!(f, "NOTPRESENT"),
+            Self::Down => write!(f, "DOWN"),
+            Self::LowerLayerDown => write!(f, "LOWERLAYERDOWN"),
+            Self::Testing => write!(f, "TESTING"),
+            Self::Dormant => write!(f, "DORMANT"),
+            Self::Up => write!(f, "UP"),
+            Self::Other(v) => write!(f, "{v}"),
+        }
+    }
+}
+
+impl std::str::FromStr for State {
+    type Err = DecodeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            s if s.eq_ignore_ascii_case("unknown") => Ok(Self::Unknown),
+            s if s.eq_ignore_ascii_case("notpresent") => Ok(Self::NotPresent),
+            s if s.eq_ignore_ascii_case("down") => Ok(Self::Down),
+            s if s.eq_ignore_ascii_case("lowerlayerdown") => {
+                Ok(Self::LowerLayerDown)
+            }
+            s if s.eq_ignore_ascii_case("testing") => Ok(Self::Testing),
+            s if s.eq_ignore_ascii_case("dormant") => Ok(Self::Dormant),
+            s if s.eq_ignore_ascii_case("up") => Ok(Self::Up),
+            _ => Err(format!("Invalid operstate: {s}").into()),
         }
     }
 }
 
 impl From<State> for u8 {
     fn from(value: State) -> Self {
-        use self::State::*;
         match value {
-            Unknown => IF_OPER_UNKNOWN,
-            NotPresent => IF_OPER_NOTPRESENT,
-            Down => IF_OPER_DOWN,
-            LowerLayerDown => IF_OPER_LOWERLAYERDOWN,
-            Testing => IF_OPER_TESTING,
-            Dormant => IF_OPER_DORMANT,
-            Up => IF_OPER_UP,
-            Other(other) => other,
+            State::Unknown => IF_OPER_UNKNOWN,
+            State::NotPresent => IF_OPER_NOTPRESENT,
+            State::Down => IF_OPER_DOWN,
+            State::LowerLayerDown => IF_OPER_LOWERLAYERDOWN,
+            State::Testing => IF_OPER_TESTING,
+            State::Dormant => IF_OPER_DORMANT,
+            State::Up => IF_OPER_UP,
+            State::Other(other) => other,
         }
     }
 }
